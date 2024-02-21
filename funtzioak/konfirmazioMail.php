2e5071911db00f82d81a091561c4c154f89d32c8
Unax Azpirotz <1ag3.unaxazpi@tolosaldealh.eus>
2024-02-21 12:16:26 +0100

    Froga mail

--- funtzioak/konfirmazioMail.php
+++ funtzioak/konfirmazioMail.php
@@ -14,7 +14,7 @@
        <mj-text align="center" font-size="16px" font-family="open Sans Helvetica, Arial, sans-serif" padding-left="25px" padding-right="25px">Mesedez, erabili beheko egiaztapen-kodea Arenguren webgunean:</mj-text>
        <mj-text align="center" font-size="24px" background-color="#20c997" font-weight="bold" font-family="open Sans Helvetica, Arial, sans-serif">234967</mj-text>
        <mj-text align="center" font-size="16px" font-family="open Sans Helvetica, Arial, sans-serif" padding-left="25px" padding-right="16px">Ez baduzu eskatu, mezu elektroniko honi jaramonik egin edo jakinarazi iezaguzu.</mj-text>
-       <mj-text align="center" font-size="16px" font-family="open Sans Helvetica, Arial, sans-serif" padding-left="25px" padding-right="25px">Mila esker! <br />Arengu team</mj-text>
+       <mj-text align="center" font-size="16px" font-family="open Sans Helvetica, Arial, sans-serif" padding-left="25px" padding-right="25px">Mila esker! <br />Tolosako Pilota Elkartea</mj-text>
      </mj-column>
    </mj-section>
  </mj-body>
