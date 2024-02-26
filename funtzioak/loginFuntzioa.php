92c47633bea8ca9d958cac818756cac6f4af16af
Cristian Mulleras <1ag3.crismull@tolosaldealh.eus>
2024-02-26 13:48:52 +0100

    Login errorea kudeatuta

--- funtzioak/loginFuntzioa.php
+++ funtzioak/loginFuntzioa.php
@@ -1,34 +1,40 @@
 <?php
     include "db.php";
-    $username_form=$_POST['username_form'];
-    $password_form=$_POST['password_form'];
-    $sql = "SELECT * FROM `erabiltzailea` WHERE username = '$username_form' AND pasahitza ='$password_form'";
-    $connection= connection();
-    $query = mysqli_query($connection, $sql);
-    if(mysqli_num_rows($query)>0){
-        while($row = mysqli_fetch_array($query)){
-            $izena=$row["izena"];
-            $abizena=$row["abizenak"];
-            $email=$row["email"];
-            $helbidea=$row["helbidea"];
-            $tlf=$row["telefonoa"];
-            $username=$row["username"];
-            $password=$row["pasahitza"];
-            $aktibo=$row["aktibo"];
-            $administratzailea=$row["administratzailea"];
-        };
-
-        session_start();
-        $_SESSION['username']=$username;
-        $_SESSION['izena']=$izena;
-        $_SESSION['abizena']=$abizena;
-        $_SESSION['email']=$email;
-        $_SESSION['tlf']=$tlf;
-        $_SESSION['admin']=$administratzailea;
-
-        header("location: ../index.php");
+    if (empty($_POST["username_form"]) || empty($_POST["password_form"])) {
+        echo 'Erabiltzailea eta pasahitza eremuak bete behar dituzu.';
     }
-    else{
-        header('location ../login.php?errorea="erabiltzale edo pasahitza okerra"');
-    }
+    else {
+        $username_form=$_POST['username_form'];
+        $password_form=$_POST['password_form'];
+        $sql = "SELECT * FROM `erabiltzailea` WHERE username = '$username_form' AND pasahitza ='$password_form'";
+        $connection= connection();
+        $query = mysqli_query($connection, $sql);
+    
+
+        if(mysqli_num_rows($query)>0){
+            while($row = mysqli_fetch_array($query)){
+                $izena=$row["izena"];
+                $abizena=$row["abizenak"];
+                $email=$row["email"];
+                $helbidea=$row["helbidea"];
+                $tlf=$row["telefonoa"];
+                $username=$row["username"];
+                $password=$row["pasahitza"];
+                $aktibo=$row["aktibo"];
+                $administratzailea=$row["administratzailea"];
+            };
+
+            session_start();
+            $_SESSION['username']=$username;
+            $_SESSION['izena']=$izena;
+            $_SESSION['abizena']=$abizena;
+            $_SESSION['email']=$email;
+            $_SESSION['tlf']=$tlf;
+            $_SESSION['admin']=$administratzailea;
+
+            header("location: ../index.php");
+        }
+        else{
+            echo 'Erabiltzailea eta pasahitza eremuak ez dira zuzenak.';
+        }
 ?>
